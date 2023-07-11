46bda977694592d8a4e67084efab9f70110206c7
DavNej <david.nejar@gmail.com>
2023-07-11 17:59:00 +0200

    config setup and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,22 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'mona-sans': ['Mona-sans', 'sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: '#FFFFFF',
+        blue: '#3632FE',
+        purple: '#8840FF',
+        pink: '#FF22BB',
+        gray: {
+          900: '#060201',
+        },
       },
+      backgroundImage: {
+        'sign-up': "url('/bg-signup.svg')",
+      }
     },
   },
   plugins: [],
