b6d66b744294d82c931d085a4b58fdbbc07caa49
DavNej <david.nejar@gmail.com>
2023-07-27 11:14:28 +0200

    feat: GPT config

---
+++ lib/chat-gpt.ts
@@ -0,0 +1,49 @@
+import { Configuration, OpenAIApi } from 'openai'
+import type { ChatCompletionRequestMessage } from 'openai'
+
+import { parseApiError } from './error-utils'
+
+export type MessageHistory = Array<ChatCompletionRequestMessage>
+
+const configuration = new Configuration({
+  organization: process.env.ORGANISATION,
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+export async function askGPT(prompt: string) {
+  try {
+    const defaultResponse = {
+      message: null,
+      error: null,
+    }
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: `You are an excellent talent advocate and mentor. You will help talents with creating an extraordinarily attractive bio for their profiles based on their skills and the basic information they provide. The bio must be concise and clear. Make it bullet point style with emojis. The user will provide you with keywords or a simple bio and you will generate 3 options from which they can select.`,
+        },
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+    })
+
+    const responseMessage = response.data.choices[0].message
+
+    if (!responseMessage)
+      return {
+        ...defaultResponse,
+        error: { message: 'AI Service not available for the moment' },
+      }
+
+    return { message: responseMessage, error: null }
+  } catch (_error) {
+    const error = parseApiError(_error)
+    return { error, message: null }
+  }
+}
