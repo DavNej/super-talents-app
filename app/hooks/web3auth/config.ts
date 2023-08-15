509d3c9914d54cb7f3455f3f6c29629203ec0d28
DavNej <david.nejar@gmail.com>
2023-08-15 21:02:21 +0200

    add: RPC_TARGET env var

--- app/hooks/web3auth/config.ts
+++ app/hooks/web3auth/config.ts
@@ -3,18 +3,18 @@ import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider'
 import { Web3AuthNoModal } from '@web3auth/no-modal'
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter'
 
+const RPC_TARGET = process.env.NEXT_PUBLIC_JSON_RPC_TARGET as string
+const WEB3AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID as string
+
 export const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
   chainId: '0x13881', // hex of 80001, polygon testnet
-  rpcTarget: 'https://endpoints.omniatech.io/v1/matic/mumbai/public',
-  // rpcTarget: 'https://rpc.ankr.com/polygon_mumbai',
+  rpcTarget: RPC_TARGET,
   displayName: 'Polygon Mumbai Testnet',
   blockExplorer: 'https://mumbai.polygonscan.com/',
   ticker: 'MATIC',
   tickerName: 'Matic',
 }
-
-const WEB3AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID as string
 
 export const web3auth = new Web3AuthNoModal({
   clientId: WEB3AUTH_CLIENT_ID,
