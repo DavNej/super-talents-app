5ebd29116af687ef3cc49f41e318126ef4607052
DavNej <david.nejar@gmail.com>
2023-08-18 14:37:06 +0200

    RQ.TalentLayer add options arg in hooks

--- lib/talent-layer/hooks.ts
+++ lib/talent-layer/hooks.ts
@@ -1,9 +1,13 @@
+import type { UseMutationOptions, UseQueryOptions } from '@tanstack/react-query'
 import { useQuery } from '@tanstack/react-query'
 
 import { getTalentLayerUser, handleExists } from './subgraph'
 import type { ITalentLayerUser, IFetchUserParams } from './types'
 
-export function useTalentLayerUser({ handle, address, id }: IFetchUserParams) {
+export function useTalentLayerUser(
+  { handle, address, id }: IFetchUserParams,
+  options?: UseQueryOptions<ITalentLayerUser | undefined>
+) {
   return useQuery<ITalentLayerUser | undefined>({
     queryKey: ['user', { handle, address, id }],
     queryFn: () =>
@@ -12,12 +16,17 @@ export function useTalentLayerUser({ handle, address, id }: IFetchUserParams) {
         address,
         id,
       }),
+    ...options,
   })
 }
 
-export function useHandleExists(handle: string) {
+export function useHandleExists(
+  { handle }: { handle: string },
+  options?: UseQueryOptions<boolean>
+) {
   return useQuery<boolean>({
     queryKey: ['handleExists'],
     queryFn: () => handleExists(handle),
+    ...options,
   })
 }
