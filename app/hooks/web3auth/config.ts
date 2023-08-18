b6268358ba7853adb76a62afe09b763120304eea
DavNej <david.nejar@gmail.com>
2023-08-18 10:48:29 +0200

    web3auth: add network in config

--- app/hooks/web3auth/config.ts
+++ app/hooks/web3auth/config.ts
@@ -5,6 +5,9 @@ import { OpenloginAdapter } from '@web3auth/openlogin-adapter'
 
 const RPC_TARGET = process.env.NEXT_PUBLIC_RPC_TARGET as string
 const WEB3AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID as string
+const WEB3AUTH_NETWORK = process.env.NEXT_PUBLIC_WEB3AUTH_NETWORK as
+  | 'testnet'
+  | 'mainnet'
 
 export const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
@@ -19,6 +22,7 @@ export const chainConfig = {
 export const web3auth = new Web3AuthNoModal({
   clientId: WEB3AUTH_CLIENT_ID,
   chainConfig,
+  web3AuthNetwork: WEB3AUTH_NETWORK,
 })
 
 const privateKeyProvider = new EthereumPrivateKeyProvider({
