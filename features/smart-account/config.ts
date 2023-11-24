8c276947e781a2d9921ba8f69f0ae534e30f903f
DavNej <david.nejar@gmail.com>
2023-11-24 17:44:22 +0100

    remove biconomy env var

--- features/smart-account/config.ts
+++ features/smart-account/config.ts
@@ -11,20 +11,19 @@ import {
 const BICONOMY_BUNDLER_URL =
   'https://bundler.biconomy.io/api/v2/80001/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44'
 
-const BICONOMY_PAYMASTER_URL = process.env.BICONOMY_PAYMASTER_URL as string
+const BICONOMY_PAYMASTER_URL =
+  'https://paymaster.biconomy.io/api/v1/80001/DqNBUdjR_.ad347838-4e86-4fe4-a47d-415dabee1d3b'
 
 export const chainId = ChainId.POLYGON_MUMBAI
 
 export const bundler: IBundler = new Bundler({
-  bundlerUrl:
-    'https://bundler.biconomy.io/api/v2/80001/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44',
+  bundlerUrl: BICONOMY_BUNDLER_URL,
   chainId,
   entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
 })
 
 export const paymaster: IPaymaster = new BiconomyPaymaster({
-  paymasterUrl:
-    'https://paymaster.biconomy.io/api/v1/80001/DqNBUdjR_.ad347838-4e86-4fe4-a47d-415dabee1d3b',
+  paymasterUrl: BICONOMY_PAYMASTER_URL,
 })
 
 export const paymasterServiceData: SponsorUserOperationDto = {
