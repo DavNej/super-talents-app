014d041ecd52a5550465dbe3f5a575208f97cd11
DavNej <david.nejar@gmail.com>
2023-11-28 20:20:03 +0100

    RQ default error handling

--- app/components/ReactQueryProvider.tsx
+++ app/components/ReactQueryProvider.tsx
@@ -1,50 +1,37 @@
 'use client'
 
 import React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { toast } from 'react-toastify'
 import { log } from '@/utils'
 
-function defaultOnError(err: unknown, variables?: unknown, context?: unknown) {
-  log('============')
-
-  console.error(err)
-
-  if (typeof err === 'string') {
-    toast.error(err, { toastId: 'root' })
-  }
-
-  if (variables) {
-    console.error({ variables })
-  }
-
-  if (context) {
-    console.error({ context })
-  }
-  log('============')
+function onError(err: unknown) {
+  const isTypeError = err instanceof Error && err.message
+  toast.error(isTypeError ? err.message : 'Something went wrong')
 }
+
+const _queryClient = new QueryClient({
+  queryCache: new QueryCache({ onError }),
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+    mutations: { onError },
+  },
+})
 
 export default function ReactQueryProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const [queryClient] = React.useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            onError: defaultOnError,
-            retry: false,
-            refetchOnWindowFocus: false,
-          },
-          mutations: {
-            onError: defaultOnError,
-          },
-        },
-      })
-  )
+  const [queryClient] = React.useState(_queryClient)
 
   return (
     <QueryClientProvider client={queryClient}>
