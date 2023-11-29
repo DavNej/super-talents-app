40b89193502e9a8db360ae3a40ad441cd17516e5
DavNej <david.nejar@gmail.com>
2023-11-29 11:53:40 +0100

    api.miint-profile update route

--- app/api/mint-profile/route.ts
+++ app/api/mint-profile/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server'
 import { ethers } from 'ethers'
 
 import {
-  profileIdOfHandle,
   platfromId,
   talentLayerAddress,
   talentLayerInterface,
@@ -13,94 +12,36 @@ import { log } from '@/utils'
 const PK = process.env.SUPERTALENTS_FUNDER_PRIVATE_KEY as string
 const RPC_TARGET = process.env.NEXT_PUBLIC_RPC_TARGET as string
 
+const provider = new ethers.providers.JsonRpcProvider(RPC_TARGET)
+const wallet = new ethers.Wallet(PK, provider)
+const contract = new ethers.Contract(
+  talentLayerAddress,
+  talentLayerInterface,
+  wallet
+)
+
 export async function POST(request: Request) {
-  console.log('🦋 | HOST', request.headers.get('host'))
-  console.log('🦋 | RPC_TARGET', RPC_TARGET)
-
-  const body = await request.json()
-  //TODO typecheck body with zod
-  const { handle, address } = body
-
-  log('📓 | Check if handle is available')
-  let profileId = await profileIdOfHandle(handle)
-
-  if (profileId !== null)
-    return NextResponse.json(
-      `TL handle "${handle}" taken => id: ${profileId}`,
-      { status: 400 }
-    )
-
-  const provider = new ethers.providers.JsonRpcProvider(RPC_TARGET)
-  const wallet = new ethers.Wallet(PK, provider)
-  const contract = new ethers.Contract(
-    talentLayerAddress,
-    talentLayerInterface,
-    wallet
-  )
-
-  let handlePrice: ethers.BigNumber
-  let txHash: string
+  const { handle, value, address } = await request.json()
 
   try {
-    log(`📓 | Get handle price of ${handle}`)
-    handlePrice = await contract.getHandlePrice(handle)
-    log('📓 | handlePrice', handlePrice)
-  } catch (err) {
-    console.error('💥', err)
-    return NextResponse.json(
-      { message: 'Could not get handle price' },
-      { status: 500 }
-    )
-  }
-
-  try {
-    log('📓 | Send mint tx', {
-      address,
-      platfromId,
-      handle,
-      value: handlePrice,
-    })
+    log('🎫 | Minting TL handle', { address, platfromId, handle, value })
 
     const mintTx: ethers.ContractTransaction = await contract.mintForAddress(
       address,
       platfromId,
       handle,
-      { value: handlePrice }
+      { value }
     )
 
-    const mintTxReceipt = await mintTx.wait(1)
-    log('📓 | TL profile minted')
-    txHash = mintTxReceipt.transactionHash
-    console.log('🤝 | Transaction hash', mintTxReceipt.transactionHash)
+    const receipt = await mintTx.wait(1)
+    const txHash = receipt.transactionHash
+    log('🎫 | TL handle minted', txHash)
+    return NextResponse.json({ txHash })
   } catch (err) {
     console.error('💥', err)
     return NextResponse.json(
-      { message: 'Could not mint TL profile' },
+      { message: 'Could not mint handle', cause: err },
       { status: 500 }
     )
   }
-
-  try {
-    log('📓 | Retrieve profile id')
-    const id: ethers.BigNumber = await contract.ids(address)
-    profileId = id?.toNumber()
-    log('📓 | profileId :', profileId)
-  } catch (err) {
-    console.error('💥', err)
-    return NextResponse.json(
-      { message: 'Could not get minted Id' },
-      { status: 500 }
-    )
-  }
-
-  if (!profileId)
-    return NextResponse.json(
-      { message: 'TL profile not minted' },
-      { status: 500 }
-    )
-
-  return NextResponse.json<{ profileId: number; txHash: string }>({
-    profileId,
-    txHash,
-  })
 }
